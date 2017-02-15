a033fb1c9204c3751e263d98fb0052c8dd8984f6
Evan Busse <evanbusse@gmail.com>
2017-02-15 09:36:23 -0700

    bubble sorting correctly

--- bubble-sort.js
+++ bubble-sort.js
@@ -3,3 +3,25 @@
 //bubble sort
 
 var array = [2,4,1,8,15,7,9];
+
+function swap(arr, idx1, idx2) {
+  var val1 = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = val1;
+}
+
+function bubbleSort(arr) {
+  var swapped;
+  do{
+    swapped = false;
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] > arr[i+1]){
+        swapped = true;
+        swap(arr, i, i+1);
+      }
+    }
+  }while(swapped);
+  return arr;
+}
+
+console.log(bubbleSort(array));
